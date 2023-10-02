1ca95af7cac13b04efd8eb1f0ac306320267e7bc
Jimmy Bates <70033294+jbates35@users.noreply.github.com>
2023-10-01 19:10:16 -0700

    yellow led test meh didn't work

--- Src/main.c
+++ Src/main.c
@@ -18,7 +18,10 @@
 
 #include <stdint.h>
 
-#include "STM32H723ZG_peripherals.h"
+#include "STM32H723xx_defines.h"
+#include "STM32H723xx_gpio.h"
+
+#define YELLOW_LED (1 << 0)
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
@@ -26,6 +29,31 @@
 
 int main(void)
 {
-    /* Loop forever */
-	for(;;);
+  RCC_RegDef_t *rcc = RCC;
+  GPIOE_PCLK_EN();
+
+  GPIO_RegDef_t *gpioe = GPIOE;
+
+  //Set yellow LED for blinking later
+  gpioe->MODER |= (01 << 0);
+
+  //Shitty counter for now
+  int i = 0;
+  int yled_state = 0;
+
+  /* Loop forever */ 
+  for(;;) {
+    if(i++<300000)
+      continue;
+
+    i = 0;
+
+    if(yled_state == 0) {
+      yled_state = 1;
+      gpioe->ODR |= (1 << 0);
+    } else {
+      yled_state = 0;
+      gpioe->ODR &= ~(1 << 0);
+    }
+  }
 }
